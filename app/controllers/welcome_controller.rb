22b112c1f5962125f93d60a10494e24953e865bc
Nabil <nabilalsouhily@gmail.com>
2017-01-25 14:30:23 +0800

    complete the authorization

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,6 @@ class WelcomeController < ApplicationController
 	def index
 # posts = Post.last
         # posts = Post.all.order('created_at DESC')
-       posts =  current_user.posts.last(2)
+       # posts =  current_user.posts.last(2)
 	end
 end
