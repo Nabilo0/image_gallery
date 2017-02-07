e73f507aadb3712d1aadd30c57ddf02d42d9aaae
Nabil <nabilalsouhily@gmail.com>
2017-02-07 20:38:49 +0800

    fixing image

--- config/initializers/carrierwave_aws.rb
+++ config/initializers/carrierwave_aws.rb
@@ -5,9 +5,9 @@
     provider:              'AWS',                        # required
     aws_access_key_id:     ENV['AWSAccessKeyId'],                        # required
     aws_secret_access_key: ENV['AWSSecretKey'],                        # required
-    region:                'us-east-1'                  # optional, defaults to 'us-east-1' ap-southeast-1
+    region:                'us-west-2'                  # optional, defaults to 'us-east-1' ap-southeast-1
   }
-  config.fog_directory  = 'imagegallery2017'                          # required
+  config.fog_directory  = 'appimagegallery'                          # required
   config.fog_public     = true                                        # optional, defaults to true
   config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
 
